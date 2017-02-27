27575fee52b43c0cf6d0d4bf5159f5c230a2f609
Andrew Stromme <andrew.stromme@gmail.com>
2017-02-27 10:19:39 -0800

    bump version to 1.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
+    version='1.1.0',
 
     description='Converts mandarin chinese .srt files to pinyin-annotated .dfxp files that can be used with Netflix.',
     long_description=long_description,
